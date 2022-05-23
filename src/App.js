07d54e1380a78bdca1a2b1d976cdde0dc0b9064e
Sebastian Zuviria <szuviria@gmail.com>
2022-05-23 17:59:29 -0300

    componentes 2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React, { useState } from 'react'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Counter from './components/Counter/Counter';
+import ClassCounter from './components/ClassCounter/ClassCounter'
 
 const App = () => {
-  const [quantity, setQuantity] = useState(0)
-
-  const handleOnAdd = (count) => {
-    setQuantity(count)
-  }
+  const [show, setShow] = useState(true)
 
   return (
     <div className="App">
       <NavBar />
-      <h1>{quantity}</h1>
-      <Counter initial={10} color='red' onAdd={handleOnAdd}/>
+      <button onClick={() => setShow(!show)}>No mostrar contador</button>
+      {/* <h1>{quantity}</h1> */}
+      {show && <Counter initial={10} color='red'/>}
+      {/* <ClassCounter /> */}
       {/* <h1 style={{ color: 'red' }}>Hola Mundo</h1>
       <button onClick={() => console.log('Aprete el boton')}>Boton</button> */}
     </div>    
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,28 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Button from '../Button/Button'
 
 const Count  = ({ initial, color, onAdd }) => {
     const [count, setCount] = useState(initial)
+    const [title, setTitle] = useState('Titulo')
+    const [result, setResult] = useState(0)
+
+    const onResize = () => {
+        console.log('resize')
+    }
+
+    useEffect(() => {
+        console.log('ya se monto')
+        setCount((prev => prev + 1))
+        window.addEventListener('resize', onResize)
+        // setCount(prev => prev + 1)
+        return () => {
+            window.removeEventListener('resize', onResize)
+        }
+    }, [title])
+
+    useEffect(() => {
+        // setTitle(title === 'Titulo' ? 'Titulo2' : 'Titulo')
+        console.log('cambio el estado count')
+        setResult(count * 2)
+    }, [count])
+
 
     const decrement = () => {
-        setCount(count - 1)
+        for(let i = 0; i < 2; i++) {
+            setCount((prev) => prev - 1)
+        }
     }
 
     const increment = () => {
         setCount(count + 1)
     }
+    console.log(count)
+    console.log('antes de montar')
 
     return(
         <div>
-            {/* <button onClick={decrement}>-</button> */}
-            <Button handleClick={decrement}>
+            <h1>{result}</h1>
+            <button onClick={decrement}>-</button>
+            {/* <Button handleClick={decrement}>
                 Restar
-            </Button>
+            </Button> */}
             <div style={{ color }}>{count}</div>
-            {/* <button onClick={increment}>+</button> */}
-            <Button handleClick={increment}>
+            <button onClick={increment}>+</button>
+            {/* <Button handleClick={increment}>
                 Sumar
-            </Button>
+            </Button> */}
             <button onClick={() => onAdd(count)}>Agregar cantidad</button>
         </div>
     )
